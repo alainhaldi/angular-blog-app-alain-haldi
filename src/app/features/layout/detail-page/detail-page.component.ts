09c41df64010ab701e6fc1dfc6fde06e4ea2842b
alainhaldi <122995199+alainhaldi@users.noreply.github.com>
2024-11-10 10:13:49 +0100

    Adding receiving Blog Data via getRequest

--- src/app/features/layout/detail-page/detail-page.component.ts
+++ src/app/features/layout/detail-page/detail-page.component.ts
@@ -1,42 +1,52 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { BlogEntry, DataService } from '../../../data.service';
+import { Observable } from 'rxjs';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-detail-page',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './detail-page.component.html',
   styleUrl: './detail-page.component.scss'
 })
 export class DetailPageComponent implements OnInit{
   blogID: number | undefined;
-  currentBlog: BlogEntry | undefined;
+  blog$!: Observable<BlogEntry>; 
 
-  dummyBlog: BlogEntry = {
-            id: 265,
-            title: 'Quarkus: With nothing you are more productive!',
-            likedByMe: false,
-            author: 'alice',
-            headerImageUrl: "https://picsum.photos/id/320/800/200",
-            likes: 1,
-            comments: 0,
-            createdAt: '',
-            contentPreview: 'This is a dummy text'
-  };
+  // dummyBlog: BlogEntry = {
+  //           id: 265,
+  //           title: 'Quarkus: With nothing you are more productive!',
+  //           likedByMe: false,
+  //           author: 'alice',
+  //           headerImageUrl: "https://picsum.photos/id/320/800/200",
+  //           likes: 1,
+  //           comments: 0,
+  //           createdAt: '',
+  //           contentPreview: 'This is a dummy text'
+  // };
+  // blog$: Observable<any> | undefined; // Observable für Blog-Daten
 
   constructor(
-    private route: ActivatedRoute,
+    // private route: ActivatedRoute,
     private dataService: DataService 
   ) {}
 
   ngOnInit(): void {
-    this.route.paramMap.subscribe(params => {
-      this.blogID = Number(params.get('id'));
-      // this.currentBlog = this.dataService.loadBlogByID(265);
+    
+    this.blog$ = this.dataService.getBlogById(265);
+    console.log(`Recieved: ${this.blog$}`)
 
-      // this.loadBlogContent(this.blogId);
-    });
+    // console.log('~~~~~~~~~~~~~~~')
+    // this.route.paramMap.subscribe(params => {
+    //   this.blogID = Number(params.get('id'));
+    //   // this.currentBlog = this.dataService.loadBlogByID(265);
+    //   const id = +this.route.snapshot.paramMap.get('id')!; // Holt die Blog-ID aus der URL
+    //   this.blog$ = this.dataService.getBlogById(id); 
+    //   console.log(this.blog$)// HTTP-Request an den Service
+    //   // this.loadBlogContent(this.blogId);
+    // });
   }
 
   loadBlogContent(id: number): void {
