e1347ee96a5bc08676e1ec08032f4bb4c401145a
alainhaldi <122995199+alainhaldi@users.noreply.github.com>
2024-11-18 21:09:37 +0100

    Implementing Signals

--- src/app/features/layout/detail-page/detail-page.component.ts
+++ src/app/features/layout/detail-page/detail-page.component.ts
@@ -12,20 +12,36 @@ import { CommonModule } from '@angular/common';
   styleUrl: './detail-page.component.scss'
 })
 export class DetailPageComponent implements OnInit{
-  blogId!: string;
-  blog$!: Observable<BlogEntry>; 
+  // blogId!: string;
+  // blog$!: Observable<BlogEntry>; 
 
-  constructor(
-    private route: ActivatedRoute,
-    private dataService: DataService 
-  ) {}
+  // constructor(
+  //   private route: ActivatedRoute,
+  //   private dataService: DataService 
+  // ) {}
+
+  // ngOnInit(): void {
+  //   // Get ID from pathurl
+  //   this.blogId = this.route.snapshot.paramMap.get('id') ?? '265'; // If NULL then default value
+  
+  //   // get blog
+  //   this.blog$ = this.dataService.getBlogById(this.blogId);
+  //   console.log(`Recieved: ${this.blog$}`)
+  // }
+
+  blogId!: string;
+
+  constructor(private dataService: DataService,private route: ActivatedRoute,) {}
 
   ngOnInit(): void {
     // Get ID from pathurl
-    this.blogId = this.route.snapshot.paramMap.get('id') ?? '265'; // If NULL then default value
-  
-    // get blog
-    this.blog$ = this.dataService.getBlogById(this.blogId);
-    console.log(`Recieved: ${this.blog$}`)
+    this.blogId = this.route.snapshot.paramMap.get('id') ?? '264'; // If NULL then default value
+    this.dataService.getBlogById(this.blogId);
+    console.log(`Recieved: ${this.blogId}`)
+  }
+
+  // Getter to access the signal value directly
+  get blog() {
+    return this.dataService._blog();
   }
 }
